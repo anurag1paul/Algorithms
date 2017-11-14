934390078311a829f6a36f8337958132e74b4e2e
Anurag Paul <anurag1paul@gmail.com>
2017-11-14 15:12:20 +0530

    Implemented Merge Sort

---
+++ src/main/java/sorting/MergeSort.java
@@ -0,0 +1,63 @@
+package sorting;
+
+/**
+ * @author Anurag Paul(anurag.paul@delhivery.com)
+ *         Date: 14/11/17
+ */
+public class MergeSort {
+
+    private static int[] merge(int[] a, int[] b){
+
+        int len_merge = a.length + b.length;
+        int[] c = new int[len_merge];
+
+        for(int k=0, i=0, j=0; k<len_merge; k++) {
+
+            if(i < a.length && j < b.length) {
+
+                if(a[i] < b[j]) {
+                    c[k] = a[i];
+                    i++;
+                }
+                else {
+                    c[k] = b[j];
+                    j++;
+                }
+            }
+            else if (i >= a.length) {
+                c[k] = b[j];
+                j++;
+            }
+            else {
+                c[k] = a[i];
+                i++;
+            }
+        }
+
+        return c;
+    }
+
+    public static int[] sort(int[] input) {
+        if (input.length > 1) {
+            int split = input.length / 2;
+            int[] front, back;
+            front = new int[split];
+            back = new int[input.length - split];
+
+            for(int i = 0, j=0, k=0; i < input.length; i++) {
+                if(i < split) {
+                    front[j] = input[i];
+                    j++;
+                }
+                else {
+                    back[k] = input[i];
+                    k++;
+                }
+            }
+
+            return merge(sort(front), sort(back));
+        }
+        else
+            return input;
+    }
+}
---
+++ src/test/java/sorting/MergeSortTest.java
@@ -0,0 +1,31 @@
+package sorting;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Anurag Paul(anurag.paul@delhivery.com)
+ *         Date: 14/11/17
+ */
+public class MergeSortTest {
+
+    @Test
+    public void sortTest(){
+        int[] input1 = {1,3,2,6,4,8,3,9,2};
+        check(input1);
+
+        int[] input2 = {1,3,2,6,4,8,3,9,2,12};
+        check(input2);
+
+        int[] input3= {9,8,7,6,5,4,3,2,1};
+        check(input3);
+    }
+
+    private void check(int[] input) {
+        int[] output = MergeSort.sort(input);
+        Assert.assertTrue(output.length == input.length);
+
+        for(int i=0; i<(output.length -1); i++)
+            Assert.assertTrue(output[i] <= output[i+1]);
+    }
+}
